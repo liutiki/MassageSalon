ac3b071b4b5c1ca272acf978a0bb512d8c8833f2
liutiki <liutiki@list.ru>
2024-05-26 09:39:12 +0300

    fixed countdown

--- src/Countdown/CountdownTimer.js
+++ src/Countdown/CountdownTimer.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './Countdown.css';
 
 const CountdownTimer = ({ targetDate }) => {
-  const calculateTimeLeft = useCallback(() => {
-    const difference = +new Date(targetDate) - +new Date();
-    let timeLeft = {};
 
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      };
-    }
+const calculateTimeLeft = useCallback(() => {
+  const difference = +new Date(targetDate) - +new Date();
+  let timeLeft = {};
 
-    return timeLeft;
-    });
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+}, [targetDate]);
+
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
